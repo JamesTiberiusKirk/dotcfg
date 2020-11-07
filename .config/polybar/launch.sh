bc889f65096e901bdef37beac50932818ce3ebcf
Dumitru Vulpe <dumitru.v.dv@gmail.com>
2020-11-07 20:54:41 +0000

    Added polybar config

---
+++ .config/polybar/launch.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Kill all polybars
+killall -q polybar
+
+# Wait until kill confirmed.
+while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+
+polybar mybar &
+
+echo "Polybar launched..."
