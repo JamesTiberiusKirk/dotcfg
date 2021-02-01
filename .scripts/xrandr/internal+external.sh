26da9f8dd2d04bc0de6819a66981c29a5f26a658
Dumitru Vulpe <dumitru.v.dv@gmail.com>
2021-02-01 15:16:11 +0000

    Update

--- .scripts/xrandr/internal+external.sh
+++ .scripts/xrandr/internal+external.sh
@@ -1,2 +1,6 @@
 #!/bin/sh
-xrandr --output DP1 --auto --brightness 0.9 --output eDP1 --mode 3200x1800 --right-of DP1
+
+~/.config/polybar/launch.sh
+
+#xrandr --output DP1 --auto --brightness 0.9 --output eDP1 --mode 3200x1800 --right-of DP1
+xrandr --output DP1 --auto --brightness 0.9 --output eDP1 --mode 1920x1080 --right-of DP1
