4367d0d9e1f3daf504f46a816c067c493fd043ab
Dumitru Vulpe <dumitru.v.dv@gmail.com>
2021-02-01 14:11:41 +0000

    Update

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -6,6 +6,15 @@ killall -q polybar
 # Wait until kill confirmed.
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-polybar mybar &
+if type "xrandr"; then
+    for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+        #MONITOR=$m polybar --reload example &
+        MONITOR=$m polybar --reload mybar &
+    done
+else
+    #polybar --reload example &
+    polybar --reload mybar &
+fi
+
 
 echo "Polybar launched..."
--- .scripts/xrandr/internal+external.sh
+++ .scripts/xrandr/internal+external.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output DP1 --auto --brightness 0.9 --output eDP1 --mode3200x1800 --right-of DP1
+xrandr --output DP1 --auto --brightness 0.9 --output eDP1 --mode 3200x1800 --right-of DP1
