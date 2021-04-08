104fafa8ab27a31e92d6d46f44fc52593c280aa3
Dumitru Vulpe <dumitru.v.dv@gmail.com>
2021-04-08 19:43:31 +0100

    Fixed wallpaper.sh

--- .scripts/wallpapers.sh
+++ .scripts/wallpapers.sh
@@ -3,5 +3,5 @@
 screen1w=$(ls $WALL_PATH/ |sort -R |tail -$N | head -n 1 )
 screen2w=$(ls $WALL_PATH/ |sort -R |tail -$N | head -n 1 )
 
-xwallpaper --output DP1 --zoom $WALL_PATH/$screen1w  --output eDP1 --zoom ~/Pictures/wallpapers/$screen2w
+xwallpaper --output DP1 --zoom $WALL_PATH/$screen1w  --output eDP1 --zoom $WALL_PATH/$screen2w
 
