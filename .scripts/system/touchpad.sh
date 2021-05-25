9c9974e58659a14f604225c8d0df1bfd545085ca
Dumitru Vulpe <dumitru.v.dv@gmail.com>
2021-05-25 14:10:41 +0100

    Updated the scripts folder structure and added a touchpad settings script

---
+++ .scripts/system/touchpad.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+# These are my touchpad settings, found it easier to do it this way rather than with xorg.conf.d config files
+
+# Use xinput list to get device id
+DEVICE_ID=16
+
+# This turns on natural scrolling
+xinput set-prop $DEVICE_ID 290 1
+
+# This sets the click type to two and three fingers rather than areas
+xinput set-prop $DEVICE_ID 312 0 1
