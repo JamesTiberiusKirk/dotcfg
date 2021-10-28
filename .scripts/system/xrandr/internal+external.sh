59458fe33ab1aec21d551a941c34792cb9da095c
Dumitru Vulpe <dumitru.v.dv@gmail.com>
2021-10-28 20:24:21 +0000

    Changes to mon script

--- .scripts/system/xrandr/internal+external.sh
+++ .scripts/system/xrandr/internal+external.sh
@@ -1,18 +1,15 @@
 #!/bin/sh
 
-xrandr --output DP-2
-PREV_DIS=DP-2
-
-echo "Available displays"
-xrandr -q | grep " connected" | cut -d " " -f1 | while read DIS;
-do 
-  #[[ -z $PREV_DIS ]] && xrandr --output $DIS --auto ;
-  [[ $DIS == "DP-2" ]] && continue;
-  [[ ! -z $PREV_DIS ]] && xrandr --output $DIS --auto --right-of $PREV_DIS;
-  echo "2:$PREV_DIS";
-  echo "1:$DIS";
-  PREV_DIS=$DIS;
-done 
+#[[ $HOST == "legion" ]] && xrandr --output DP-2 --primary && PREV_DIS=DP-2 && INTERNAL=DP-2;
+#[[ $HOST == "dellstar" ]] && xrandr --output eDP1 --primary && PREV_DIS=eDP1 && INTERNAL=eDP1;
+#
+#xrandr -q | grep " connected" | cut -d " " -f1 | while read DIS;
+#do 
+#  #[[ -z $PREV_DIS ]] && xrandr --output $DIS --auto ;
+#  [[ $DIS == $INTERNAL ]] && continue;
+#  [[ ! -z $PREV_DIS ]] && xrandr --output $DIS --auto --right-of $PREV_DIS;
+#  PREV_DIS=$DIS;
+#done 
 
 ~/.config/polybar/launch.sh
 #~/.scripts/wallpapers.sh
