360061e57031e174dfa828b354c588ec66d7226f
Dumitru Vulpe <dumitru.v.dv@gmail.com>
2021-11-26 10:36:02 +0000

    Added support for portrait monitors

--- .scripts/system/wallpapers.sh
+++ .scripts/system/wallpapers.sh
@@ -1,8 +1,16 @@
 #!/bin/sh
 
 
-xrandr -q | grep " connected" | grep " connected" | cut -d ' ' -f1 | while read DIS;
+xrandr -q | grep " connected" | grep " connected" | grep -v " left" | cut -d ' ' -f1 | while read DIS;
 do
-  RAND_WALL=$(ls $WALL_PATH/ |sort -R |tail -$N | head -n 1 )
+  RAND_WALL=$(ls $WALL_PATH/  | sort -R |tail -$N | head -n 1 )
   xwallpaper --output $DIS --zoom $WALL_PATH/$RAND_WALL
 done
+
+
+xrandr -q | grep " connected" | grep " left" | grep " connected" | cut -d ' ' -f1 | while read DIS;
+do
+  RAND_WALL=$(ls $WALL_PATH/ | grep "_portrait_friendly" |sort -R |tail -$N | head -n 1 )
+  xwallpaper --output $DIS --zoom $WALL_PATH/$RAND_WALL
+done
+
