78bc32024b13a0b162282bf82f0214c83f74fcc9
Dumitru Vulpe <dumitru.v.dv@gmail.com>
2021-11-26 12:58:54 +0000

    Wallpaper update

--- .scripts/system/wallpapers.sh
+++ .scripts/system/wallpapers.sh
@@ -3,7 +3,7 @@
 
 xrandr -q | grep " connected" | grep " connected" | grep -v " left" | cut -d ' ' -f1 | while read DIS;
 do
-  RAND_WALL=$(ls $WALL_PATH/  | sort -R |tail -$N | head -n 1 )
+  RAND_WALL=$(ls $WALL_PATH/  | grep -v "_only" | sort -R |tail -$N | head -n 1 )
   xwallpaper --output $DIS --zoom $WALL_PATH/$RAND_WALL
 done
 
