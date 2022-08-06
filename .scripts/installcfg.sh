5181bf60a2d81ca8ae39461abcbf7f770107efc2
Dumitru Vulpe <dumitru@xiatech.co.uk>
2022-08-06 18:01:54 +0100

    Update

--- .scripts/installcfg.sh
+++ .scripts/installcfg.sh
@@ -2,12 +2,20 @@
 
 [ -f $HOME/.xinitrc ] && mv $HOME/.xinitrc $HOME/.xinitrc.old
 
-git init --bare $HOME/.cfg
-git --git-dir=$HOME/.cfg/ --work-tree=$HOME config --local status.showUntrackedFiles no
-git --git-dir=$HOME/.cfg/ --work-tree=$HOME remote add origin git@github.com:JamesTiberiusKirk/dotcfg.git 
-git --git-dir=$HOME/.cfg/ --work-tree=$HOME fetch
-git --git-dir=$HOME/.cfg/ --work-tree=$HOME branch --set-upstream-to=origin/master master
-#git --git-dir=$HOME/.cfg/ --work-tree=$HOME fetch origin master
-git --git-dir=$HOME/.cfg/ --work-tree=$HOME pull
+rm -rf $HOME/.cfg
 
-sudo cp ~/.config/70-synaptics.conf /etc/X11/xorg.conf.d/70-synaptics.conf
+git clone --bare git@github.com:JamesTiberiusKirk/dotcfg.git $HOME/.cfg
+git --git-dir=$HOME/.cfg/ --work-tree=$HOME checkout
+git --git-dir=$HOME/.cfg/ --work-tree=$HOME config --local status.showUntrackedFiles no
+
+# Powerline10k
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+
+# packer
+git clone --depth 1 https://github.com/wbthomason/packer.nvim ~/.local/share/nvim/site/pack/packer/start/packer.nvim
+
+# Tmux tpm
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+
+[ -f /etc/X11/xorg.conf.d ] && sudo cp ~/.config/70-synaptics.conf /etc/X11/xorg.conf.d/70-synaptics.conf
+
