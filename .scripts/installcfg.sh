4148417effe58e17b793e5a444de7363aeacaff6
Dumitru Vulpe <darthvader@Dumitrus-MacBook-Pro-2.local>
2022-09-29 09:05:38 +0100

    Update

--- .scripts/installcfg.sh
+++ .scripts/installcfg.sh
@@ -2,14 +2,18 @@
 
 [ -f $HOME/.xinitrc ] && mv $HOME/.xinitrc $HOME/.xinitrc.old
 
+# ohmyzsh
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+# Powerline10k
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+
 rm -rf $HOME/.cfg
 
 git clone --bare git@github.com:JamesTiberiusKirk/dotcfg.git $HOME/.cfg
 git --git-dir=$HOME/.cfg/ --work-tree=$HOME checkout
 git --git-dir=$HOME/.cfg/ --work-tree=$HOME config --local status.showUntrackedFiles no
 
-# Powerline10k
-git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 
 # packer
 git clone --depth 1 https://github.com/wbthomason/packer.nvim ~/.local/share/nvim/site/pack/packer/start/packer.nvim
@@ -18,4 +22,6 @@ git clone --depth 1 https://github.com/wbthomason/packer.nvim ~/.local/share/nvi
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 
 [ -f /etc/X11/xorg.conf.d ] && sudo cp ~/.config/70-synaptics.conf /etc/X11/xorg.conf.d/70-synaptics.conf
+
+
 
