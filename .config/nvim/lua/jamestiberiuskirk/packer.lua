c4d0cb5c406686f9dc4564e47392a78970ec157a
Dumitru Vulpe <darthvader@Dumitrus-MacBook-Pro-2.local>
2022-10-14 21:59:00 +0100

    Update

--- .config/nvim/lua/jamestiberiuskirk/packer.lua
+++ .config/nvim/lua/jamestiberiuskirk/packer.lua
@@ -53,12 +53,12 @@ return require('packer').startup(function(use)
   use 'hrsh7th/nvim-cmp'
 
   use 'tpope/vim-fugitive'
-  -- use {
-  --   'tanvirtin/vgit.nvim',
-  --   requires = {
-  --     'nvim-lua/plenary.nvim'
-  --   }
-  -- }
+  use {
+    'tanvirtin/vgit.nvim',
+    requires = {
+      'nvim-lua/plenary.nvim'
+    }
+  }
 
   --use({
   --  'ray-x/navigator.lua',
--- .config/nvim/lua/jamestiberiuskirk/set.lua
+++ .config/nvim/lua/jamestiberiuskirk/set.lua
@@ -5,11 +5,11 @@ vim.opt.relativenumber = true
 
 vim.opt.errorbells = false
 
-vim.opt.tabstop = 2
-vim.opt.softtabstop = 2
-vim.opt.shiftwidth = 2
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
-vim.opt.scrolloff = 8
+vim.opt.scrolloff = 0
 vim.opt.mouse = "a"
 
 vim.opt.smartindent = true
