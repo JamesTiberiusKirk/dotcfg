a8027ac134ecc9d4d5be9aec61f96f61c423973d
Dumitru Vulpe <dumitru.v.dv@gmail.com>
2022-11-13 05:23:24 +0000

    Update

--- .config/nvim/lua/jamestiberiuskirk/packer.lua
+++ .config/nvim/lua/jamestiberiuskirk/packer.lua
@@ -3,6 +3,8 @@ vim.cmd [[packadd packer.nvim]]
 return require('packer').startup(function(use)
   use 'wbthomason/packer.nvim'
 
+  use 'christoomey/vim-tmux-navigator'
+
   -- Theme
   use 'folke/tokyonight.nvim'
   use 'rafi/awesome-vim-colorschemes'
