759f92c0901c4565466d04633bc85adc59522484
Dumitru Vulpe <dumitru.v.dv@gmail.com>
2022-11-15 08:58:51 +0000

    System changes

--- .scripts/system/xrandr/internal+external.sh
+++ .scripts/system/xrandr/internal+external.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
 # xrandr --output DP-2 --auto --output HDMI-0 --auto --right-of DP-2 --output DP-0.1 --auto --rotate left --right-of HDMI-0 ;
-xrandr --output DP-2 --auto --output HDMI-0 --auto --left-of DP-2;
-sleep 1s;
-~/.scripts/system/wallpapers.sh;
-sleep 0.5s;
+xrandr --output DP-2 --auto --output HDMI-0 --auto --left-of DP-2
+sleep 1s
+~/.scripts/system/wallpapers.sh
+sleep 0.5s
 ~/.config/polybar/launch.sh >> /dev/null
 ~/.scripts/system/set_keyboard.sh
--- .scripts/system/xrandr/internal_monitor.sh
+++ .scripts/system/xrandr/internal_monitor.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-xrandr --output HDMI-0 --off --output DP-2 --auto;
+xrandr --output HDMI-0 --off --output DP-2 --auto
 ~/.config/polybar/launch.sh &
