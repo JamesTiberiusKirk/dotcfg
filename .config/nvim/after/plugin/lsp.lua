f2172df026d73f8bf0b8ceb297f5981b08d6a003
Dumitru Vulpe <dumitru.v.dv@gmail.com>
2022-11-15 13:45:12 +0000

    Update

--- .config/nvim/after/plugin/lsp.lua
+++ .config/nvim/after/plugin/lsp.lua
@@ -31,18 +31,12 @@ require("nvim-lsp-installer").setup({
     }
 })
 
--- require('navigator.lspclient.mapping').setup()
-
 local nnoremap = require("jamestiberiuskirk.keymap").nnoremap
--- nnoremap("gd","<cmd>lua vim.lsp.buf.declaration()<cr>")
-nnoremap("gd","<cmd>lua vim.lsp.buf.definition()<cr>")
--- go to deginition in a split and vertical split bellow
 nnoremap("sgd","<cmd>sp<cr><C-w>j<cmd>lua vim.lsp.buf.definition()<cr>")
 nnoremap("vgd","<cmd>vsp<cr><C-w>l<cmd>lua vim.lsp.buf.definition()<cr>")
-
 nnoremap("gi","<cmd>lua vim.lsp.buf.implementation()<cr>")
 nnoremap("gr","<cmd>lua vim.lsp.buf.reference()<cr>")
 nnoremap("<leader>g","<cmd>lua vim.lsp.buf.hover()<cr>")
 nnoremap("<leader>r","<cmd>lua vim.lsp.buf.rename()<cr>")
 nnoremap("<leader>do","<cmd>lua vim.lsp.buf.action()<cr>")
-nnoremap("err","<cmd>lua vim.lsp.diagnostic.show_line_diagnostics()<cr>")
+nnoremap("err","<cmd>lua vim.diagnostic.open_float({scope='line'})<cr>")
