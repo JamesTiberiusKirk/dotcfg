0bab06e314eb71b20ce0d69f6b7466c5143d6004
Dumitru Vulpe <dumitru.v.dv@gmail.com>
2022-11-21 13:14:10 +0000

    Telescope update

--- .config/nvim/after/plugin/telescope.lua
+++ .config/nvim/after/plugin/telescope.lua
@@ -1,54 +1,40 @@
 require('telescope').setup{
-defaults = {
-    -- Default configuration for telescope goes here:
-    -- config_key = value,
+  defaults = {
     mappings = {
-        i = {
-            -- map actions.which_key to <C-h> (default: <C-/>)
-            -- actions.which_key shows the mappings for your picker,
-            -- e.g. git_{create, delete, ...}_branch for the git_branches picker
-            ["<C-h>"] = "which_key"
-        }
+      i = {
+        ["<C-h>"] = "which_key"
+      }
+
     }
-},
-pickers = {
-    -- Default configuration for builtin pickers goes here:
-    -- picker_name = {
-    --   picker_config_key = value,
-    --   ...
-    -- }
-    -- Now the picker_config_key will be applied every time you call this
-    -- builtin picker
+  },
+  pickers = {
     find_files = {
-        -- file_ignore_pattern = "vendor",
-        find_command  = {
-            "fd",
-            "--no-ignore",
-            "--type",
-            "f",
-            "--unrestricted",
-            "-H",
-            "-E",
-            "db-data",
-            "-E",
-            "vendor",
-            "-E",
-            ".git"
-        },
+      find_command  = {
+        "fd",
+        "--no-ignore",
+        "--type",
+        "f",
+        "--unrestricted",
+        "-H",
+        "-E",
+        "db-data",
+        "-E",
+        "*vendor*",
+        "-E",
+        "vendor",
+        "-E",
+        "*node_modules*",
+        "-E",
+        ".git"
+      },
     }
-},
-extensions = {
-    -- Your extension configuration goes here:
-    -- extension_name = {
-    --   extension_config_key = value,
-    -- }
-    -- please take a look at the readme of the extension you want to configure
-}
+  },
+  extensions = {
+  }
 }
 
 local nnoremap = require("jamestiberiuskirk.keymap").nnoremap
 nnoremap("<leader>o", "<cmd>lua require('telescope.builtin').git_branches()<cr>")
--- nnoremap("<leader>p", "<cmd>lua require('telescope.builtin').git_files()<cr>")
 nnoremap("<leader>p", "<cmd>lua require('telescope.builtin').find_files()<cr>")
 nnoremap("<leader>f", "<cmd>lua require('telescope.builtin').live_grep()<cr>")
 nnoremap("<leader>v", "<cmd>lua require('telescope.builtin').buffers()<cr>")
