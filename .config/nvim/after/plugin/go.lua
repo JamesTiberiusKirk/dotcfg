3bf727f42437c30d8fa97a70cbf9e80b64f23723
JamesTiberiusKirk <dumitru.v.dv@gmail.com>
2023-01-28 08:20:17 +0000

    Update

--- .config/nvim/after/plugin/go.lua
+++ .config/nvim/after/plugin/go.lua
@@ -1,7 +1,7 @@
 vim.cmd("let g:go_fmt_command = 'goimports'")
-vim.cmd("au BufRead,BufNewFile *.html set filetype=gohtmltmpl")
+-- vim.cmd("au BufRead,BufNewFile *.html set filetype=gohtmltmpl")
 vim.cmd("au BufRead,BufNewFile *.gohtml set filetype=gohtmltmpl")
-vim.cmd("au BufRead,BufNewFile *.gohtml set filetype=html")
+-- vim.cmd("au BufRead,BufNewFile *.gohtml set filetype=html")
 
 local nnoremap = require("jamestiberiuskirk.keymap").nnoremap
 nnoremap("gc","<cmd>GoCoverage<CR>")
