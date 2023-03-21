9dc65a48de13fd45cbd8c3a510dbd5c422cf323c
JamesTiberiusKirk <dumitru.v.dv@gmail.com>
2023-03-21 10:09:59 +0000

    Update

---
+++ .config/nvim/lua/plugins/whichkey.lua
@@ -0,0 +1,11 @@
+-- within plugins/which-key.lua
+return {
+  -- 'folke/which-key.nvim',
+  -- config = function()
+  --   local wk = require('which-key')
+  --   wk.register({
+  --     ['<leader>'] = {
+  --     },
+  --   })
+  -- end,
+}
