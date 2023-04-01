188f49f45707ca0c97c1c35e425d75939d2550cb
JamesTiberiusKirk <dumitru.v.dv@gmail.com>
2023-04-01 22:32:16 +0100

    Update

--- .config/nvim/lua/plugins/todo.lua
+++ .config/nvim/lua/plugins/todo.lua
@@ -17,7 +17,7 @@ return {
         PERF = { icon = " ", alt = { "OPTIM", "PERFORMANCE", "OPTIMIZE" } },
         NOTE = { icon = " ", color = "hint", alt = { "INFO" } },
         TEST = { icon = "⏲ ", color = "test", alt = { "TESTING", "PASSED", "FAILED" } },
-        WTF = { icon = " ", color = "warning" },
+        WTFJS = { icon = " ", color = "warning" },
       },
     },
   -- stylua: ignore
